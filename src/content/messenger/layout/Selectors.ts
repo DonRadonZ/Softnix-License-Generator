5e5a95085ac0c5857b691e44585bb42696f4660c
RadonZ <supachai27102000@gmail.com>
2022-07-14 00:42:01 +0700

    update_some_Info_can_use

--- src/content/messenger/layout/Selectors.ts
+++ src/content/messenger/layout/Selectors.ts
@@ -1,31 +1,31 @@
-// import { AppState } from './../../../component/countermessage/storeTypes';
+ import { AppState } from './../../../component/countermessage/storeTypes';
 import { createSelector } from "reselect";
 import { View } from "./LayoutReducer";
 
-// const getLayoutSlice = (state: AppState) => state.layout;
+const getLayoutSlice = (state: AppState) => state.layout;
 
-// export const getView = createSelector(
-//     [getLayoutSlice],
-//     (app: ReturnType<typeof getLayoutSlice>) => {
-//         return app.views[0];
-//     }
-//     );
+export const getView = createSelector(
+    [getLayoutSlice],
+    (app: ReturnType<typeof getLayoutSlice>) => {
+        return app.views[0];
+    }
+    );
 
-// export const getViews = createSelector(
-//     [getLayoutSlice],
-//     (app: ReturnType<typeof getLayoutSlice>) => {
-//         return app.views;
-//     }
-// );
+export const getViews = createSelector(
+    [getLayoutSlice],
+    (app: ReturnType<typeof getLayoutSlice>) => {
+        return app.views;
+    }
+);
 
-// export const getViewStates = createSelector(
-//     [getViews],
-//     (views: ReturnType<typeof getViews>) => {
-//         return {
-//             Menu: views.includes(View.Menu),
-//             ConversationMembers: views.includes(View.ConversationMembers),
-//             JoinConversation: views.includes(View.JoinConversation),
-//             CurrentConversation: views.includes(View.CurrentConversation)
-//         };
-//     }
-// );
+export const getViewStates = createSelector(
+    [getViews],
+    (views: ReturnType<typeof getViews>) => {
+        return {
+            Menu: views.includes(View.Menu),
+            ConversationMembers: views.includes(View.ConversationMembers),
+            JoinConversation: views.includes(View.JoinConversation),
+            CurrentConversation: views.includes(View.CurrentConversation)
+        };
+    }
+);
