9214ebde19640f8e1be393c2898be607fff5a58a
RadonZ <supachai27102000@gmail.com>
2022-07-19 14:04:00 +0700

    Add Component

--- src/content/messenger/foundations/component/layout/flex-column/flex-column.tsx
+++ src/content/messenger/foundations/component/layout/flex-column/flex-column.tsx
@@ -3,11 +3,11 @@ import { flexbox, FlexboxProps, layout, LayoutProps } from "styled-system";
 import { StyledBox } from "../styled-box/styled-box";
 
 /** CSS ultilities wrapper that also displays children in flex-column layout */
-export const FelxColumn = Styled(StyledBox)<FlexboxProps& LayoutProps>`
+export const FlexColumn = Styled(StyledBox)<FlexboxProps& LayoutProps>`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 
   ${flexbox}
   ${layout}
-`
+`;
