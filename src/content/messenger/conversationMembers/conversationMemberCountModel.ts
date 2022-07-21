0218ca3d8fd15f2e8f214ce45bdc81ce1ddeb948
RadonZ <supachai27102000@gmail.com>
2022-07-22 00:47:58 +0700

    Finish Conversation Count Model

--- src/content/messenger/conversationMembers/conversationMemberCountModel.ts
+++ src/content/messenger/conversationMembers/conversationMemberCountModel.ts
@@ -1,24 +1,24 @@
  import { AppState } from './../../../component/countermessage/storeTypes';
-// import { createSelector } from "reselect";
-//  import { createChannelMembersCountReducer } from "pubnub-redux";
+import { createSelector } from "reselect";
+ import { createChannelMembersCountReducer } from "pubnub-redux";
 
-// export  interface ConversationMembersCount {
-//     [conversationId: string]: number;
-// }
+export  interface ConversationMembersCount {
+    [conversationId: string]: number;
+}
 
-// const getMembersCountByConversationIdSlice = (state: AppState) =>
-//   state.conversationMembersCount;
+const getMembersCountByConversationIdSlice = (state: AppState) =>
+  state.conversationMembersCount;
 
-// export const getMembersCountByConversationId = createSelector(
-//     [getMembersCountByConversationIdSlice],
-//     (users: {byId: ConversationMembersCount}) => {
-//         return users.byId;
-//     }
-// );
+export const getMembersCountByConversationId = createSelector(
+    [getMembersCountByConversationIdSlice],
+    (users: {byId: ConversationMembersCount}) => {
+        return users.byId;
+    }
+);
 
-// const ConversationMembersCountStateReducer = createChannelMembersCountReducer();
+const ConversationMembersCountStateReducer = createChannelMembersCountReducer();
 
-// export {ConversationMembersCountStateReducer};
+export {ConversationMembersCountStateReducer};
 
 
 
