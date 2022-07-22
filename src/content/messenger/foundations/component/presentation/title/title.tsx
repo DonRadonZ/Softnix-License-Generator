3d7ac99d8a4de3546c2e4d8fa91fa581b58d3ecc
RadonZ <supachai27102000@gmail.com>
2022-07-22 18:12:35 +0700

    Add Component

---
+++ src/content/messenger/foundations/component/presentation/title/title.tsx
@@ -0,0 +1,51 @@
+import React, { FunctionComponent, HTMLAttributes } from "react";
+import Styled from "styled-components/macro";
+import {
+    Heading,
+    HeadingProps,
+    Label,
+    LabelProps
+  } from "../Index"
+
+  interface TitleWrapperProps extends HTMLAttributes<HTMLElement> {
+    /** Should the Heading and Label be capitalized */
+    capitalize?: boolean;
+  }
+
+  interface TitleProps extends TitleWrapperProps {
+    /** Text passed to the Heading */
+    heading?: string;
+
+     /** Options passed to the Heading */
+     headingProps?: HeadingProps;   
+
+     /** Text passsed to the Label */
+     label?: string;
+
+     /** Options passed to the Label */
+     labelProps?: LabelProps;
+  }
+
+  export const TitleWrapper = Styled.div<TitleWrapperProps>`
+  * {
+    text-transform: ${p => p.capitalize && "capitalize"}
+  }
+  ${Heading} {
+    margin-bottom: 5px;
+  }
+`;
+
+export const Title: FunctionComponent<TitleProps> = ({
+    heading,
+    headingProps,
+    label,
+    labelProps,
+    ...rest
+}) => {
+    return(
+        <TitleWrapper {...rest}>
+        {heading && <Heading {...headingProps}></Heading>}
+        {label && <Label {...labelProps}>{label}</Label>}
+        </TitleWrapper>
+    );
+};
